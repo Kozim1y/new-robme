eb234dd6af9c45a5f3d6e6cc578bc4b87d1bd2cb
Usta_Kozim <87965501+usta-kozim@users.noreply.github.com>
2023-05-31 16:06:25 +0500

    Change all side bar

--- js/main.js
+++ js/main.js
@@ -9,6 +9,7 @@ for (let i = 0; i < elDragItems.length; i++) {
 	item.addEventListener("dragstart", function () {
 		draggedItem = item;
 		setTimeout(function () {
+			item.style.transition = "all 0.3s ease"
 			item.style.display = "none";
 		}, 0);
 	});
